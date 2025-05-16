5de4e3c9967db3590b2234937ec987ffc303d58f
R Morse <rikmorse@gmail.com>
2025-05-15 19:45:37 -0700

    update about html
    about.html update

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+  });
+  
+  var about = "I was born and raised in Reno, NV. I am a junior software developer with a passion for learning and growing in the field of technology. I have experience in various programming languages and frameworks, and I am always eager to take on new challenges. In my free time, I enjoy hiking, reading, and spending time with my family and friends.";
+  document.getElementById("about").innerHTML = about; 
+
 var today = new Date();
 var hours = today.getHours();
 var greeting;
@@ -15,9 +22,11 @@ else {
 console.log(greeting);
 
 document.getElementById("greeting").innerHTML = greeting;
-;
+
 document.getElementById("nameInput").addEventListener("input", function() {
     const name = document.getElementById("nameInput").value;
     document.getElementById("greeting").innerHTML = greeting + " " + name + ":)";
 }
 );
+
+
